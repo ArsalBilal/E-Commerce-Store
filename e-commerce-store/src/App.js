99aaed57e32c8840c8f94a11f39a7e3d22d8b5b9
Arsal Bilal <jountyroud@gmail.com>
2025-07-29 13:31:25 +0500

    Removed unused import

--- e-commerce-store/src/App.js
+++ e-commerce-store/src/App.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Products from './components/modules/product/Product'; 
 import './styles/App.css';
