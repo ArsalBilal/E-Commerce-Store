33e944eb529c7451339f068b86bf02136ce59122
arsalbial14 <jountyroud@gmail.com>
2025-08-12 12:15:28 +0500

    `Added unused import statement and updated import list`

--- e-commerce-store/src/components/base/SessionManager.tsx
+++ e-commerce-store/src/components/base/SessionManager.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState, ReactNode } from "react";
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { Navigate, useLocation, useNavigate } from "react-router-dom";
 import { refreshToken } from "../../services/auth";
 
